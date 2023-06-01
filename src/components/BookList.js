90b284956c1009f6cdd347c5edc5aa3fde35dbfa
souravsa89 <greyersa@gmail.com>
2023-06-01 17:09:02 +0530

    change

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -11,13 +11,7 @@ const BookList = () => {
       })}
     </div>
   ) : (
-    <div className="card text-center">
-      <div className="card-header">Book List</div>
-      <div className="card-body">
-        <h5 className="card-title">Currently No Listing</h5>
-        <p className="card-text">Add Your Reading List.</p>
-      </div>
-    </div>
+    
   );
 };
 export default BookList;
